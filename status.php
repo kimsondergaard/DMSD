e9fb8fcc3a3538c38d8f4cdbd2d64e637002ae0b
kimsondergaard <kim@kimsondergaard.dk>
2014-11-03 12:01:49 +0100

    ....

--- status.php
+++ status.php
@@ -1,6 +1,99 @@
 <head>
 <link href="print.css" type="text/css" rel="stylesheet" media="print">
 <title>Printer status</title>
+
+<style type="text/css">
+
+/*------------------------------------------------------------------
+Table Style
+------------------------------------------------------------------ */
+table a:link {
+    color: #666;
+    font-weight: bold;
+    text-decoration:none;
+}
+table a:visited {
+    color: #999999;
+    font-weight:bold;
+    text-decoration:none;
+}
+table a:active,
+table a:hover {
+    color: #bd5a35;
+    text-decoration:underline;
+}
+table {
+    font-family:Arial, Helvetica, sans-serif;
+    color:#666;
+    font-size:12px;
+    background:#eaebec;
+    border:#ccc 1px solid;
+
+    border-radius:3px;
+    border-collapse:collapse; border-spacing: 0; 
+
+    box-shadow: 0 1px 2px #d1d1d1;
+    min-width: 1000px;
+}
+table th {
+    padding:10px 10px 10px 10px;
+    border-top:0;
+    border-bottom:1px solid #e0e0e0;
+    border-left: 1px solid #e0e0e0;
+
+    background: #ededed;
+}
+table th:first-child {
+    text-align: left;
+}
+table tr:first-child th:first-child {
+    border-top-left-radius:3px;
+    border-left: 0;
+}
+table tr:first-child th:last-child {
+    border-top-right-radius:3px;
+}
+table tr {
+    text-align: center;
+}
+table td:first-child {
+    text-align: left;
+    border-left: 0;
+}
+table td {
+    padding:10px;
+    border-bottom:1px solid #e0e0e0;
+    border-left: 1px solid #e0e0e0;
+    background: #fafafa;
+}
+table tr:last-child td {
+    border-bottom:0;
+}
+table tr:last-child td:first-child {
+    border-bottom-left-radius:3px;
+}
+table tr:last-child td:last-child {
+    border-bottom-right-radius:3px;
+}
+table tr:hover td {
+    background: #f2f2f2;
+
+}
+table th, table td {
+    width: 160px;
+
+}
+#wrapper {
+    width: 740px;
+    height: 200px;
+    overflow-x: scroll;
+    overflow-y: scroll;
+}
+table thead {
+    position: fixed;
+}
+</style>
+
 </head>
 <html>
 <body style="font-family: century gothic; font-size: 13px; background-image: url('http://p1.pichost.me/i/5/1282860.jpg'); background-repeat: no-repeat; background-attachment: fixed; color: white; ">
